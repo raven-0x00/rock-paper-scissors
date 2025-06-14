f8ceea449882dd81bd2b0a634ddb5e3534fc9492
raven-0x00 <132075815+raven-0x00@users.noreply.github.com>
2025-06-14 20:00:49 +0300

    Code main game logic before major update to game logic

---
+++ javascript.js
@@ -0,0 +1,97 @@
+// Human and computer scores initialization
+let humanScore = 0;
+let computerScore = 0;
+
+// Generate a random number between 0-3
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// Convert the randomly generated number to a choice in the game (Computer's choice) and store it for later usage.
+function getComputerChoice(){
+    let outPut = getRandomInt(3);
+
+    if (outPut === 0) {
+        return "rock"
+    } else if (outPut === 1) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
+
+let computerChoice = getComputerChoice()
+
+// Get human choice and store in a variable
+
+let humanChoice = prompt("Rock, paper, or scissors?")
+
+function getHumanChoice(choice) {
+    choice = choice.toLocaleLowerCase();
+    return choice;
+}
+
+humanChoice = getHumanChoice(humanChoice);
+
+// Implement game logic (round logic)
+
+function playRound(humanChoice, computerChoice) {
+    let winner = null;
+    switch (humanChoice) {
+        case "rock":
+            if (computerChoice === humanChoice) {
+                winner = "tie"
+            } else if (computerChoice === "paper") {
+                computerScore = ++computerScore;
+                winner = "computer";
+            } else {
+                humanScore = ++humanScore;
+                winner = "human";
+            }
+            break;
+        case "scissors":
+            if (computerChoice === humanChoice) {
+                winner = "tie"
+            } else if (computerChoice === "rock") {
+                computerScore = ++computerScore;
+                winner = "computer";
+            } else {
+                humanScore = ++humanScore;
+                winner = "human";
+            }
+            break;
+        case "paper":
+            if (computerChoice === humanChoice) {
+                winner = "tie"
+            } else if (computerChoice === "scissors") {
+                computerScore = ++computerScore;
+                winner = "computer";
+            } else {
+                humanScore = ++humanScore;
+                winner = "human";
+            }
+            break;
+    }
+    return winner;
+}
+
+// Implement entire game logic
+function playRound() {
+    
+}
+
+
+
+
+
+
+
+
+// let winner = playRound(humanChoice, computerChoice);
+// if (winner === "human") {
+//     console.log(`You win! ${humanChoice} beats ${computerChoice}, your score: ${humanScore}, computer score: ${computerScore}`)
+// } else if (winner === "computer") {
+//     console.log(`You lose! ${computerChoice} beats ${humanChoice}, your score: ${humanScore}, computer score: ${computerScore}`)
+// } else {
+//     console.log(`It's a tie!, your score: ${humanScore}, computer score: ${computerScore}`)
+// }
